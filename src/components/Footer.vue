d65dfa5daf5b12d545ecaa459d349e0ed49fddbf
vincezap <vince@zapproach.com>
2022-08-15 22:24:30 +0400

    Final changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 
 <div class="footer bg-emerald-900 rounded text-center py-2 bottom-0 fixed w-full">
-  <p class="text-teal-600 font-bold">Made with love &#10084;&#65039; By <a href="" class="text-emerald-500 hover:text-gray-800 transition-colors">vincedotcode</a></p> 
+  <p class="text-teal-600 font-bold">Made with love &#10084;&#65039; By <a href="https://vincecodes.netlify.app/" class="text-emerald-500 hover:text-gray-800 transition-colors">vincedotcode</a></p> 
 </div>
 
 
